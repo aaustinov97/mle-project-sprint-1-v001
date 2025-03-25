1d31e15bdb353fd69da6f99f4b42d0175812dd33
aaustinov97 <aaustinov97@yandex.ru>
2025-03-25 13:07:56 +0000

    Подготовлены скрипты для dvc

---
+++ part2_dvc/scripts/evaluate.py
@@ -0,0 +1,34 @@
+from sklearn.model_selection import cross_validate, KFold
+import yaml
+import joblib
+import pandas as pd
+import os
+import json
+
+def evaluate_model():
+    with open('part2_dvc/params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+    
+    pipeline = joblib.load('part2_dvc/models/fitted_model.pkl')
+    data = pd.read_csv('part2_dvc/data/initial_data.csv')
+    
+    cv_strategy = KFold(n_splits=params['n_splits'], shuffle=params['shuffle'], random_state=params['random_seed'])
+    cv_res = cross_validate(
+        pipeline,
+        data,
+        data[params['target_col']],
+        cv=cv_strategy,
+        n_jobs=params['n_jobs'],
+        scoring=params['scoring']
+    )
+    
+    for key, value in cv_res.items():
+        cv_res[key] = round(value.mean(), 3)
+
+    os.makedirs('part2_dvc/cv_results', exist_ok=True)
+    with open('part2_dvc/cv_results/cv_res.json', 'w') as f:
+        json.dump(cv_res, f)
+
+
+if __name__ == '__main__':
+    evaluate_model()
---
+++ part2_dvc/scripts/fit.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import yaml
+import os
+import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler
+from catboost import CatBoostRegressor
+
+
+def fit_model():
+    with open('part2_dvc/params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+
+    data = pd.read_csv('part2_dvc/data/initial_data.csv')
+
+    cat_features = data.select_dtypes(include='object')
+    potential_binary_features = cat_features.nunique() == 2
+    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
+
+    one_hot_encoder_features = pd.concat([binary_cat_features, data[['building_type_int']]], axis=1)
+    min_max_columns = data[['build_age', 'floor', 'rooms', 'floors_total']]
+    
+    num_features = data.select_dtypes(['float'])
+    num_features.drop(columns=['target',], inplace=True)
+
+    preprocessor = ColumnTransformer(
+        [
+            ('one_hot_enc', OneHotEncoder(drop=params['one_hot_drop']), one_hot_encoder_features.tolist()),
+            ('minmax', MinMaxScaler(), min_max_columns.columns.tolist()),
+            ('num', StandardScaler(), num_features.columns.tolist())
+        ],
+        remainder='drop',
+        verbose_feature_names_out=False
+    )
+
+    model = CatBoostRegressor(
+                iterations=params['iterations'], 
+                depth=params['depth'], 
+                learning_rate=params['learning_rate'], 
+                l2_leaf_reg=params['l2_leaf_reg'], 
+                random_seed=params['random_seed'],
+                verbose=params['verbose']
+                )
+
+    pipeline = Pipeline(
+        [
+            ('preprocessor', preprocessor),
+            ('model', model)
+        ]
+    )
+    pipeline.fit(data, data[params['target_col']])
+    os.makedirs('part2_dvc/models', exist_ok=True)
+    joblib.dump(pipeline, "part2_dvc/models/fitted_model.pkl")
+
+if __name__ == '__main__':
+    fit_model()
